447e2a99306a879d978e4cd3dbfb5f9ff4b278e1
ManuelTorres-11 <jesusmanueltorresescalante@gmail.com>
2024-02-25 15:21:37 -0700

    php variables

---
+++ variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+$y = "John";
+
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+<?php
+$txt = "W3Schools.com";
+echo "I love $txt!";
+?>
+
+</body>
+</html>
