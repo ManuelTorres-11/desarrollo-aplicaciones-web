f9c4c07fd3acf4ed2f4aaee0927a9b1b283a310c
ManuelTorres-11 <jesusmanueltorresescalante@gmail.com>
2024-02-25 15:54:32 -0700

    php math

---
+++ math.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo(pi());
+?>
+
+<?php
+echo(min(0, 150, 30, 20, -8, -200) . "<br>");
+echo(max(0, 150, 30, 20, -8, -200));
+?>
+
+<?php
+echo(round(0.60) . "<br>");
+echo(round(0.50) . "<br>");
+echo(round(0.49) . "<br>");
+echo(round(-4.40) . "<br>");
+echo(round(-4.60));
+?>
+
+</body>
+</html>
