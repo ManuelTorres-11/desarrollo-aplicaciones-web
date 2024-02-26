78ee16ed30c87b315b07a4c11dd11670a312141a
ManuelTorres-11 <jesusmanueltorresescalante@gmail.com>
2024-02-25 17:24:13 -0700

    php operators

---
+++ operators.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 10;  
+$y = 6;
+
+echo $x + $y;
+?>  
+
+<?php
+$x = 10;  
+$y = 6;
+
+echo $x * $y;
+?>  
+
+<?php
+$x = 20;  
+$x += 100;
+
+echo $x;
+?>  
+
+<?php
+$x = 100;  
+$y = "100";
+
+var_dump($x !== $y); // returns true because types are not equal
+?>  
+
+</body>
+</html>
